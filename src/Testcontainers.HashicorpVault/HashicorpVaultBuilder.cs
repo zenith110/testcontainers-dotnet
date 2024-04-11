4817a484f0ed91318975f06c9d3e7431ec25d915
zenith110 <thequake@live.com>
2024-04-10 20:34:13 -0400

    Removed leftover boilerplate

--- src/Testcontainers.HashicorpVault/HashicorpVaultBuilder.cs
+++ src/Testcontainers.HashicorpVault/HashicorpVaultBuilder.cs
@@ -14,16 +14,6 @@ public sealed class HashicorpVaultBuilder : ContainerBuilder<HashicorpVaultBuild
     public HashicorpVaultBuilder()
         : this(new HashicorpVaultConfiguration())
     {
-        // 1) To change the ContainerBuilder default configuration override the DockerResourceConfiguration property and the "HashicorpVaultBuilder Init()" method.
-        //    Append the module configuration to base.Init() e.g. base.Init().WithImage("alpine:3.17") to set the modules' default image.
-
-        // 2) To customize the ContainerBuilder validation override the "void Validate()" method.
-        //    Use Testcontainers' Guard.Argument<TType>(TType, string) or your own guard implementation to validate the module configuration.
-
-        // 3) Add custom builder methods to extend the ContainerBuilder capabilities such as "HashicorpVaultBuilder WithHashicorpVaultConfig(object)".
-        //    Merge the current module configuration with a new instance of the immutable HashicorpVaultConfiguration type to update the module configuration.
-
-        // DockerResourceConfiguration = Init().DockerResourceConfiguration;
     }
 
     /// <summary>
@@ -35,21 +25,6 @@ public sealed class HashicorpVaultBuilder : ContainerBuilder<HashicorpVaultBuild
     {
         DockerResourceConfiguration = resourceConfiguration;
     }
-
-    // /// <inheritdoc />
-    // protected override HashicorpVaultConfiguration DockerResourceConfiguration { get; }
-
-    // /// <summary>
-    // /// Sets the HashicorpVault config.
-    // /// </summary>
-    // /// <param name="config">The HashicorpVault config.</param>
-    // /// <returns>A configured instance of <see cref="HashicorpVaultBuilder" />.</returns>
-    // public HashicorpVaultBuilder WithHashicorpVaultConfig(object config)
-    // {
-    //     // Extends the ContainerBuilder capabilities and holds a custom configuration in HashicorpVaultConfiguration.
-    //     // In case of a module requires other properties to represent itself, extend ContainerConfiguration.
-    //     return Merge(DockerResourceConfiguration, new HashicorpVaultConfiguration(config: config));
-    // }
 
     /// <inheritdoc />
     public override HashicorpVaultContainer Build()
